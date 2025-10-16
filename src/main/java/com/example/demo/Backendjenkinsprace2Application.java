c885b7cd599b8b4ecfe62a99f433cf2a815945ee
Shruthika <shruthikapadma2004@gmail.com>
2025-10-16 10:00:20 +0530

    gfds

---
+++ src/main/java/com/example/demo/Backendjenkinsprace2Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Backendjenkinsprace2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Backendjenkinsprace2Application.class, args);
+	}
+
+}
